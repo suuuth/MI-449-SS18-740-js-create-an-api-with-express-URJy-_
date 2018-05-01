cca33f529797620e102e5eb38c0401228f4fdb37
suuuth <suther98@msu.edu>
2018-05-01 16:50:58 -0400

    adding complete checker

--- todo.js
+++ todo.js
@@ -1,14 +1,17 @@
 module.exports = {
     'clean-room': {
       name: 'clean your room',
-      time_minutes: '20'
+      time_minutes: '20',
+      complete: 'no'
     },
     'buy-food': {
       name: 'go to the grocery',
-      time_minutes: '120'
+      time_minutes: '120',
+      complete: 'no'
     },
     'make-bed': {
       name: 'make your bed',
-      time_minutes: '5'
+      time_minutes: '5',
+      complete: 'no'
     }
   }
